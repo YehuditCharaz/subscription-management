9b3ece35f4137a2c9514126b7b2848602c2da338
Y. Charaz <yeuditc@skyvar.co.il>
2024-02-26 11:15:21 +0200

    check linter

--- services/emails/func-emails/project/download_excel.py
+++ services/emails/func-emails/project/download_excel.py
@@ -8,23 +8,29 @@ import config.config_variables
 
 
 def download_blob_excel(blob_name):
-    secret = config.config_variables.email_secret
-    connection_string = (
-        project.get_connection_string.get_connection_string_from_keyvault(secret)
-    )
-    container_name = "excel"
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
-    blob_client = container_client.get_blob_client(blob_name)
-    blob_data = blob_client.download_blob().readall()
-    return blob_data
+    try:
+        secret = config.config_variables.email_secret
+        connection_string = (
+            project.get_connection_string.get_connection_string_from_keyvault(secret)
+        )
+        container_name = "excel"
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(blob_name)
+        blob_data = blob_client.download_blob().readall()
+        return blob_data
+    except Exception as ex:
+        return str(ex)
 
 
 def delete_blob_excel(container_name, blob_name):
-    secret = config.config_variables.email_secret
-    connection_string = (
-        project.get_connection_string.get_connection_string_from_keyvault(secret)
-    )
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
-    container_client.delete_blob(blob_name)
+    try:
+        secret = config.config_variables.email_secret
+        connection_string = (
+            project.get_connection_string.get_connection_string_from_keyvault(secret)
+        )
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        container_client = blob_service_client.get_container_client(container_name)
+        container_client.delete_blob(blob_name)
+    except Exception as ex:
+        return str(ex)
--- services/subscriptions/func-subscriptions-automation/project/body_email.py
+++ services/subscriptions/func-subscriptions-automation/project/body_email.py
@@ -6,22 +6,27 @@ import config.config_variables
 
 
 def build_email_body(sub_name, sub_id, sub_activity, cost):
-    body = ""
-    if not sub_activity or cost:
-        body += f"\n subscription {sub_name} :{sub_id}"
-    if not sub_activity:
-        body += "\n The subscription has not been used for the past two weeks, if you do not log in to the subscription in the coming week, the subscription will be deleted."
-    if cost:
-        cost_set = config.config_variables.cost
-        body += f"\n The cost of the subscription is lower than {cost_set}"
-    return body
-
+    try:
+        body = ""
+        if not sub_activity or cost:
+            body += f"\n subscription {sub_name} :{sub_id}"
+        if not sub_activity:
+            body += "\n The subscription has not been used for the past two weeks, if you do not log in to the subscription in the coming week, the subscription will be deleted."
+        if cost:
+            cost_set = config.config_variables.cost
+            body += f"\n The cost of the subscription is lower than {cost_set}"
+        return body
+    except Exception as ex:
+        return str(ex)
 
 def build_email_body_to_excel(sub_activity, cost):
-    body = ""
-    if not sub_activity:
-        body += "The subscription has not been used recently."
-    if cost:
-        cost_set = config.config_variables.cost
-        body += f"The cost of the subscription is lower than {cost_set}."
-    return body
+    try:
+        body = ""
+        if not sub_activity:
+            body += "The subscription has not been used recently."
+        if cost:
+            cost_set = config.config_variables.cost
+            body += f"The cost of the subscription is lower than {cost_set}."
+        return body
+    except Exception as ex:
+        return str(ex)
