30373a8081ce8007cfc2b5b6031e429add889c80
Y. Charaz <yeuditc@skyvar.co.il>
2024-03-03 12:23:19 +0200

    :tada: init project

---
+++ users-management/app/Filament/Resources/RequestResource/Pages/ListRequests.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\RequestResource\Pages;
+
+use App\Filament\Resources\RequestResource;
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Action;
+
+class ListRequests extends ListRecords
+{
+    protected static string $resource = RequestResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+            Action::make('switchView')
+                ->label(__('change view'))
+                ->url(fn () =>request()->input('viewType', 'Table') === 'Table' ?  url()->current() . '?viewType=Card': url()->current() . '?viewType=Table')
+                ->visible(auth()->user()->role === 'Admin')
+        ];
+    }
+}
---
+++ users-management/app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Action;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    public function mount(): void
+    {
+        $user = UserResource::getUserFromAzure();
+        abort_unless($user->role === "Admin", 401);
+    }
+
+    public function getHeaderActions(): array
+    {
+        return [
+            // Actions\CreateAction::make(),
+            Action::make('switchView')
+                ->label(__('change view'))
+                ->url(fn () =>request()->input('viewType', 'Table') === 'Table' ?  url()->current() . '?viewType=Card': url()->current() . '?viewType=Table')
+        ];
+    }
+}
